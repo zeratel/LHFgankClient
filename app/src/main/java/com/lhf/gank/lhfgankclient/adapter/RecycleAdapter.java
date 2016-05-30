98b4994c4ce2766f6d44ac2563d3cdc99e81720e
zeratel3000 <liuhongfei@9fbank.cn>
2016-05-30 16:01:30 +0800

    no message

--- app/src/main/java/com/lhf/gank/lhfgankclient/adapter/RecycleAdapter.java
+++ app/src/main/java/com/lhf/gank/lhfgankclient/adapter/RecycleAdapter.java
@@ -43,8 +43,6 @@ public class RecycleAdapter extends RecyclerView.Adapter {
                 context).inflate(R.layout.normal_item, parent,
                 false));
         return holder;
-   
-
     }
 
     @Override
